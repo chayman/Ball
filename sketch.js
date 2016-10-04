96a376f4535c65c486b4fba1f39ad3ce5dd9c1e0
chayman <cvh243@nyu.edu>
2016-10-04 12:00:20 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,17 +1,31 @@
 var x=100;
-var y=100;
+var y=150;
 var speed = 5;
 var c;
+var rSlider, gSlider, bSlider;
 
 function setup() {
 createCanvas(windowWidth,windowHeight);
 c= color(10,255,150);
+
+  // create sliders
+  rSlider = createSlider(0, 255, 100);
+  rSlider.position(20, 20);
+  gSlider = createSlider(0, 255, 0);
+  gSlider.position(20, 50);
+  bSlider = createSlider(0, 255, 255);
+  bSlider.position(20, 80);
 }
 
 function draw() {
-  background(255);
-  fill(0);
-  noStroke();
+  var r = rSlider.value();
+  var g = gSlider.value();
+  var b = bSlider.value();
+  background(r, g, b);
+  text("red", 165, 35);
+  text("green", 165, 65);
+  text("blue", 165, 95);
+
     text("X coordinate is:" + x,20,20);
     
 noStroke();
@@ -19,7 +33,7 @@ fill(c);
 
 
  
-ellipse(x,y,50,50);
+ellipse(x,y,50,mouseX);
 if(x >= width-25){
  speed = -speed;
 
@@ -31,5 +45,13 @@ if (x<=25){
 
 }
 x = x + speed;
+}
+
+function keyPressed() {if (key === 'a') {
+    c = 255;
+    console.log("a");
+  } else {
+    c = 255;
+  }
 }
 
